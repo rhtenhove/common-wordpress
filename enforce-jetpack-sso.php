8e4e3c12ad39aa84db694bf30064167c46339e6b
rhtenhove <rhtenhove@users.noreply.github.com>
2020-10-21 16:41:34 +0200

    add disable update email notifications

--- enforce-jetpack-sso.php
+++ enforce-jetpack-sso.php
@@ -1,14 +1,21 @@
 <?php
 
 /**
- * Plugin Name:       Enforce Jetpack SSO
- * Plugin URI:        https://github.com/rhtenhove/enforce-jetpack-sso
- * Description:       This plugin enforces Jetpack's SSO setting
- * Version:           1
+ * Plugin Name:       Common Wordpress
+ * Plugin URI:        https://github.com/rhtenhove/common-wordpress
+ * Description:       This plugin enables some features which I'd like to have enabled on all Wordpress sites
+ * Version:           2
  * Author:            Ruben ten Hove
  * Author URI:        https://github.com/rhtenhove
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  */
 
+// Force Jetpack SSO to require two step authentication
 add_filter( 'jetpack_sso_require_two_step', '__return_true' );
+
+//Disable plugin auto-update email notification
+add_filter('auto_plugin_update_send_email', '__return_false');
+ 
+//Disable theme auto-update email notification
+add_filter('auto_theme_update_send_email', '__return_false');
